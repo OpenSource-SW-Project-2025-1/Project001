e014cc0ac1eb0b1a72e84d2be51cac5f8eaa90d1
LeeInsu <118724642+happylife10201020@users.noreply.github.com>
2025-05-16 23:18:34 +0900

    Delete Project001-feature-singin directory

--- Project001-feature-singin/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserLoginView, UserSignUpView
-
-urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('signup/', UserSignUpView.as_view(), name='signup'),
-]
