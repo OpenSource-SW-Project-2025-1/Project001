f495defb986ca992f2f5218f4a19dd2a7208a330
jaehoonkim <jaehoonnie99@gmail.com>
2025-05-24 17:29:53 +0900

    Add BOKMANI main page and base template

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import UserLoginView
-
+from django.urls import path
+from .views import main_page
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('login/', UserLoginView.as_view(), name='login'),
+    path('', main_page, name='main'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,29 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login
 from django.views.generic.edit import FormView
+from django.shortcuts import render
 
 class UserLoginView(FormView):
     template_name = 'accounts/login.html'
     form_class = AuthenticationForm
-    success_url = '/'  # 로그인 성공 시 이동할 페이지
+    success_url = '/'
 
     def form_valid(self, form):
         login(self.request, form.get_user())
         return super().form_valid(form)
+
+def main_page(request):
+    new_benefits = [
+        {"title": "다문화가족 교육비 지원", "description": "전자 바우처 제공", "d_day": 30},
+        {"title": "청년 월세 지원", "description": "1년간 최대 월 20만원", "d_day": 12},
+    ]
+
+    popular_benefits = [
+        {"title": "취업 준비금 지원", "description": "1인당 150만원", "d_day": 5},
+        {"title": "자격증 시험비 지원", "description": "최대 3회 지원", "d_day": 22},
+    ]
+
+    return render(request, 'accounts/main.html', {
+        'new_benefits': new_benefits,
+        'popular_benefits': popular_benefits
+    })
