83fb5a3c7216a6cb098a157303a3fe52570fb226
Vessis <taeyangs666@gmail.com>
2025-05-28 12:30:46 +0900

    add javascript code to signup.html

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
 from accounts.views import main_page  # accounts 앱에서 메인 화면을 가져와서
 def home(request):
     return main_page(request)         # 메인화면을 그대로 띄움
+# 회원가입 시 중복확인버튼 클릭 시 기존 사용자 데이터 불러오는 코드 만들어주세요
