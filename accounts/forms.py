7ec2972e3e0d0811fa33cfa3b70562d22b306888
Vessis <taeyangs666@gmail.com>
2025-06-04 23:59:16 +0900

    fix login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import UserID, UserProfile, UserJobInfo
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
 
 class SignUpForm(forms.ModelForm):
     class Meta:
@@ -26,3 +27,23 @@ class UserJobInfoForm(forms.ModelForm):
     class Meta:
         model = UserJobInfo
         fields = ['user_job', 'user_classification', 'user_income']
+
+#로그인 확인 폼
+class UserLoginForm(forms.Form):
+    user_id = forms.CharField(label="아이디")
+    user_pw = forms.CharField(label="비밀번호", widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user_id = cleaned_data.get("user_id")
+        user_pw = cleaned_data.get("user_pw")
+
+        try:
+            user = UserID.objects.get(user_id=user_id)
+            if not check_password(user_pw, user.user_pw):
+                raise forms.ValidationError("비밀번호가 올바르지 않습니다.")
+            cleaned_data['user'] = user
+        except UserID.DoesNotExist:
+            raise forms.ValidationError("존재하지 않는 아이디입니다.")
+
+        return cleaned_data
